19499ddb5851cc7bdd1f6940d276bf762b27a861
leeyahui <liyahui900508@163.com>
2020-09-30 16:18:38 +0800

    添加项目文件。

---
+++ admin/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+﻿<template>
+    <div>
+        <h1>to do list</h1>
+        <input type="text" v-model="inputvalue" />
+        <button @click="handlerAdd">提交</button>
+        <ul>
+            <li v-for="(item,index) in list" :key="index" @click="handlerDelete">
+                {{item}}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                inputvalue: '',
+                list: []
+            }
+        },
+        methods: {
+            handlerAdd: function () {
+                this.list.push(this.inputvalue)
+                this.inputvalue = ''
+            },
+            handlerDelete: function (index) {
+                this.list.splice(index, 1)
+            }
+        }
+    }
+</script>
